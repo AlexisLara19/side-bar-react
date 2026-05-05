3049c187944afbcaffea0eab839efa634d515ecf
AlexisLaraCal <alexis191195@hotmail.com>
2026-05-05 14:45:24 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-blue-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo eum possimus quis maiores facilis, minus saepe inventore tempora aliquam incidunt quae repellendus corrupti dolore dolorem cupiditate illo iste molestias corporis?</h1>
+    </>
+  )
+}
+
+export default App
